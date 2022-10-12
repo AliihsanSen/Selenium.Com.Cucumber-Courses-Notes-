d74f8f6864ca35c3ebd6fdc55163f970c7076aef
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-12 17:40:30 +0300

    12.10.2022

--- src/test/java/pages/PracticeAmazonPage.java
+++ src/test/java/pages/PracticeAmazonPage.java
@@ -1,2 +1,12 @@
-package pages;public class PracticeAmazonPage {
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class PracticeAmazonPage {
+
+    public PracticeAmazonPage() {
+
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 }
