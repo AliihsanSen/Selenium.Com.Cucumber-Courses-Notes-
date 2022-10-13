268633b16ecbb833cee388a905482df8f2e99730
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-13 21:07:10 +0300

    13.10.2022

--- src/test/java/stepDefinitions/GuruStepDefinition.java
+++ src/test/java/stepDefinitions/GuruStepDefinition.java
@@ -1,10 +1,24 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.And;
+import pages.GuruPage;
 
 public class GuruStepDefinition {
+
+    GuruPage guruPage = new GuruPage();
+
     @And("{string} sutunundaki tum degerleri yazdirir")
-    public void sutunundakiTumDegerleriYazdirir(String arg0) {
+    public void sutunundakiTumDegerleriYazdirir(String baslik) {
+
+        for (int i = 0; i < guruPage.basliklar.size(); i++) {
+
+            int index = 0;
+            if (guruPage.basliklar.get(i).getText().equals(baslik)) {
+                System.out.println(guruPage.basliklar.get(i).getText());
+                index = i + 1;
+            }
+        }
+
     }
 
 }
--- target/test-classes/features/US011_ClassWork.feature
+++ target/test-classes/features/US011_ClassWork.feature
@@ -3,4 +3,3 @@ Feature: US011 Class Work
   Scenario:TC01 Kullanıcı sütun basligi ile liste alabilmeli
     Given Kullanici "guruUrl" sayfasina gider
     And "Istenen Baslik" sutunundaki tum degerleri yazdirir
-    And kulllanici sayfayi kapatir
