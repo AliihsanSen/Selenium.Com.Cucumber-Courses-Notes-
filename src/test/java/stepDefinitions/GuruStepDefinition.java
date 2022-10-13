6807c96cf4348c9b106ee44da0d93695ecefcdfd
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-13 21:09:37 +0300

    13.10.2022

--- src/test/java/stepDefinitions/GuruStepDefinition.java
+++ src/test/java/stepDefinitions/GuruStepDefinition.java
@@ -1,7 +1,12 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.And;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import pages.GuruPage;
+import utilities.Driver;
+
+import java.util.List;
 
 public class GuruStepDefinition {
 
@@ -16,6 +21,9 @@ public class GuruStepDefinition {
             if (guruPage.basliklar.get(i).getText().equals(baslik)) {
                 System.out.println(guruPage.basliklar.get(i).getText());
                 index = i + 1;
+                List<WebElement> sutunElement =
+                        Driver.getDriver().findElements(By.xpath("//tbody//tr//td[" + index + "]"));
+                sutunElement.forEach(t -> System.out.println(t.getText()));
             }
         }
 
