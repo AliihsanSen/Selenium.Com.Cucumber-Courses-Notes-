4cc939ec5f94d0464a1adc5e894c027f31a79674
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-13 23:02:05 +0300

    13.10.2022

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -7,32 +7,34 @@ import utilities.ReusableMethods;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin={"html:target\\cucumber-reports.html"},
+        plugin = {"html:target\\cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"},
         features = "src/test/resources/features",
         glue = "stepDefinitions",
-        tags = "@gp6", // iki tag'ı aynı anda çalıştırmak isterseniz "@gp1 or @gp2"
+        tags = "@gp2", // iki tag'ı aynı anda çalıştırmak isterseniz "@gp1 or @gp2"
         dryRun = false
 )
 
 public class Runner extends ReusableMethods {
 
 /**
-Runner Class'ı TestNG'deki XMl mantığı ile çalışır. Çalıştırmak istediğimiz senartolara tag belirtiriz
-ve belirttiğimiz tag'ları çalıştırır. XMl deki gibi istediğimiz testleri çalıştırmak için kullanırız.
-Runner class body'si boştur ve runner class'ını ekleyeceğimiz notasyonlar aktive eder.
-Bu class'da kullanacağımız 2 adet notasyon vardır.
+ Runner Class'ı TestNG'deki XMl mantığı ile çalışır. Çalıştırmak istediğimiz senartolara tag belirtiriz
+ ve belirttiğimiz tag'ları çalıştırır. XMl deki gibi istediğimiz testleri çalıştırmak için kullanırız.
+ Runner class body'si boştur ve runner class'ını ekleyeceğimiz notasyonlar aktive eder.
+ Bu class'da kullanacağımız 2 adet notasyon vardır.
 
--@RunWith(Cucumber.class)  notasyonu Runner class'ına çalışma özelliği ekler.
-Bu notasyon olduğu için Cucumber frameworkumuzde Junit kullanmayı tercih ederiz
+ -@RunWith(Cucumber.class)  notasyonu Runner class'ına çalışma özelliği ekler.
+ Bu notasyon olduğu için Cucumber frameworkumuzde Junit kullanmayı tercih ederiz
 
--@CucumberOptions notasyonu içinde
-features : Runeer dosyasının feature dosyasını nereden bulacağını tarif eder
-glue : stepDefinitions yolunu belirtiriz
-tags : Hangi tag'i çalıştırmak istiyorsak onu belli eder
+ -@CucumberOptions notasyonu içinde
+ features : Runeer dosyasının feature dosyasını nereden bulacağını tarif eder
+ glue : stepDefinitions yolunu belirtiriz
+ tags : Hangi tag'i çalıştırmak istiyorsak onu belli eder
 
-dryRun : iki seçenek vardır
-dryRun = true; dersek testimizi çalıştırmadan eksik adımları bize verir
-dryRun = false; testlerimizi driver ile çalıştırır.
-*/
+ dryRun : iki seçenek vardır
+ dryRun = true; dersek testimizi çalıştırmadan eksik adımları bize verir
+ dryRun = false; testlerimizi driver ile çalıştırır.
+ */
 
 }
